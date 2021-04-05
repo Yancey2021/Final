13a67a699cca031da5216cd3641696ec825f4c65
Yancey2021 <77703749+Yancey2021@users.noreply.github.com>
2021-04-05 16:00:06 -0400

    Getting Started
    Start of project

---
+++ FinalOOPProject/FinalOOPProject/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalOOPProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            class Movie
+        {
+            string category;
+            string title;
+            string rating;
+        }
+        List<Movies> myMovieList = new List<Movies>();
+
+        Movie horror1 = new Movie;
+        horror1.category = "Horror";
+        horror1.title = "Ghosts of War";
+        horror1.rating = "R";
+        myMovieList.add(horror1);
+
+
+        
+
+        }
+
+        
+    }
+}
