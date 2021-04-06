1549953b235c4481c118c6f7051e9bbf72a10a78
Yancey2021 <77703749+Yancey2021@users.noreply.github.com>
2021-04-05 23:06:41 -0400

    Check in #3
    check in

--- FinalOOPProject/FinalOOPProject/Program.cs
+++ FinalOOPProject/FinalOOPProject/Program.cs
@@ -8,18 +8,15 @@ namespace FinalOOPProject
 {
     class Movie
     {
-        string category;
-        string title;
-        string rating;
+        public string category;
+        public string title;
+        public string rating;
     }
     class Program
     {
         static void Main(string[] args)
         {
-
-        
-
-        List<Movies> myMovieList = new List<Movies>();
+            List<Movie> myMovieList = new List<Movie>();
 
         Movie horror1 = new Movie;
         horror1.category = "Horror";
@@ -81,65 +78,65 @@ namespace FinalOOPProject
         comedy5.rating = "PG-13";
         myMovieList.Add(comedy5);
 
-            Movie action1 = new Movie;
-            action1.category = "Action";
-            action1.title = "Extraction";
-            action1.rating = "R";
-            myMovieList.Add(action1);
+        Movie action1 = new Movie;
+        action1.category = "Action";
+        action1.title = "Extraction";
+        action1.rating = "R";
+        myMovieList.Add(action1);
 
-            Movie action2 = new Movie;
-            action2.category = "Action";
-            action2.title = "The Outpost";
-            action2.rating = "R";
-            myMovieList.Add(action2);
+        Movie action2 = new Movie;
+        action2.category = "Action";
+        action2.title = "The Outpost";
+        action2.rating = "R";
+        myMovieList.Add(action2);
 
-            Movie action3 = new Movie;
-            action3.category = "Action";
-            action3.title = "The Old Guard";
-            action3.rating = "R";
-            myMovieList.Add(action3);
+        Movie action3 = new Movie;
+        action3.category = "Action";
+        action3.title = "The Old Guard";
+        action3.rating = "R";
+        myMovieList.Add(action3);
 
-            Movie action4 = new Movie;
-            action4.category = "Action";
-            action4.title = "Olympus Has Fallen";
-            action4.rating = "R";
-            myMovieList.Add(action4);
+        Movie action4 = new Movie;
+        action4.category = "Action";
+        action4.title = "Olympus Has Fallen";
+        action4.rating = "R";
+        myMovieList.Add(action4);
 
-            Movie action5 = new Movie;
-            action5.category = "Action";
-            action5.title = "Django Unchained";
-            action5.rating = "R";
-            myMovieList.Add(action5);
+        Movie action5 = new Movie;
+        action5.category = "Action";
+        action5.title = "Django Unchained";
+        action5.rating = "R";
+        myMovieList.Add(action5);
 
-            Movie mystery1 = new Movie;
-            mystery1.category = "Mystery";
-            mystery1.title = "It Comes At Night";
-            mystery1.rating = "R";
-            myMovieList.Add(mystery1);
+        Movie mystery1 = new Movie;
+        mystery1.category = "Mystery";
+        mystery1.title = "It Comes At Night";
+        mystery1.rating = "R";
+        myMovieList.Add(mystery1);
 
-            Movie mystery2 = new Movie;
-            mystery2.category = "Mystery";
-            mystery2.title = "Freaks";
-            mystery2.rating = "R";
-            myMovieList.Add(mystery2);
+        Movie mystery2 = new Movie;
+        mystery2.category = "Mystery";
+        mystery2.title = "Freaks";
+        mystery2.rating = "R";
+        myMovieList.Add(mystery2);
 
-            Movie mystery3 = new Movie;
-            mystery3.category = "Mystery";
-            mystery3.title = "The Ritual";
-            mystery3.rating = "R";
-            myMovieList.Add(mystery3);
+        Movie mystery3 = new Movie;
+        mystery3.category = "Mystery";
+        mystery3.title = "The Ritual";
+        mystery3.rating = "R";
+        myMovieList.Add(mystery3);
 
-            Movie mystery4 = new Movie;
-            mystery4.category = "Mystery";
-            mystery4.title = "Cam";
-            mystery4.rating = "R";
-            myMovieList.Add(mystery4);
+        Movie mystery4 = new Movie;
+        mystery4.category = "Mystery";
+        mystery4.title = "Cam";
+        mystery4.rating = "R";
+        myMovieList.Add(mystery4);
 
-            Movie mystery5 = new Movie;
-            mystery5.category = "Mystery";
-            mystery5.title = "The Discovery";
-            mystery5.rating = "R";
-            myMovieList.Add(mystery5);
+        Movie mystery5 = new Movie;
+        mystery5.category = "Mystery";
+        mystery5.title = "The Discovery";
+        mystery5.rating = "R";
+        myMovieList.Add(mystery5);
         }
 
         
